93da62ca67475ee70fb7d3d2faf3f221c876bca7
Ismail Saleh <147888888+Ismasal@users.noreply.github.com>
2025-10-06 22:51:13 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,9 +25,13 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 // serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'dist')));  // Serve built React
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "./client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 8000;
