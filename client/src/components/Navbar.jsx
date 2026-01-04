8659ad9949abc11b834d39037f9169bed1d09d78
Ismail Saleh <147888888+IsmailSaleh04@users.noreply.github.com>
2026-01-04 07:27:18 +0300

    Add useState import to Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AuthContext } from "../context/AuthContext";
